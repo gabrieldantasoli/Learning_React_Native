5ca3cb7502081e416204af123a577e9b900dbdba
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-01-28 00:37:15 -0300

    First App

--- mpa/App.tsx
+++ mpa/App.tsx
@@ -1,118 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React ,{ Component , useState } from "react";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View , Text , Image , Button , StyleSheet } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+/*
+  COMPONENTE DE CLASSE
+export default class App extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Olá , Mundo!</Text>
+      </View>
+    )
+  }
+}*/
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+export default function App() {
+  
+  return(
+    <View style={styles.area}>
+      <Text style={[styles.text,styles.align]}>USando grupo de estilos .</Text>
+      <Text style={styles.text}>Para que ?</Text>
+      <Text style={[styles.paragraph,styles.align]}>Melhor estilização e organização do codigo.</Text>
     </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  area: {
+    backgroundColor: "#b5b0b0",
+    margin: 10,
+    padding: 10
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  text: {
+    fontSize: 20,
+    color: "#000",
+    fontWeight: "bold"
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  paragraph: {
+    fontSize: 16,
+    color: "#f00",
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  align: {
+    textAlign: "center"
+  }
+})
 
-export default App;
+
+
+function useStado() {
+  const [name , setName] = useState("UNKNOWN");
+  const [age , setAge] = useState(18);
+
+  function changeName(name,age) {
+    setName(name);
+    setAge(age)
+  }
+
+  return(
+    <View>
+      <Text style={{fontSize: 30, color: "#000", margin: 10}}>Hello , {name} !</Text>
+      <Text style={{fontSize: 30, color: "#000", margin: 10}}>Age : {age}</Text>
+      <Button title="Change Name" onPress={() => changeName("GDO",20)} />
+    </View>
+  )
+}
+
+
+
+function FirstApp() {
+  let name = "Gabriel";
+  return(
+    <View style={{backgroundColor: "#cfd0b9"}}> 
+      <Text>Hello , World !</Text>
+        <Text style={{color: "red", fontSize: 25, margin: 15}}>My first App!</Text>
+        <Text style={{fontSize: 30, color:"green", fontWeight: "bold", margin: 15}}>My name is Gabriel</Text>
+        <Logo l={350} w={250}/>
+        <Text style={{margin: 10, color: "blue", fontSize: 30}}>{name}</Text>
+    </View>
+  )
+}
+
+function Logo(props) {
+
+  return(
+    <Image source={{ uri: "https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2021/06/shrek-0.jpg?quality=100&strip=info&w=1200&h=720&crop=1"}} style={{width: props.l, height: props.w, margin: "auto"}} alt="Shreck"/>
+  )
+}
