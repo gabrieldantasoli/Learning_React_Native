09d5b2086a406ce39f4ceb3a8875740a9cf823de
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-01-31 12:43:49 -0300

    Bubble and Selection

--- mpa2/App.tsx
+++ mpa2/App.tsx
@@ -1,12 +1,91 @@
 import React, { useState } from "react";
-import { View , Text , StyleSheet , ScrollView, FlatList } from "react-native";
+import { View , Text , StyleSheet , ScrollView, FlatList, Switch } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import Pessoa from "./src/Pessoa";
 import User from "./src/User";
+import Slider from "@react-native-community/slider";
 
 export default function App() {
+  const [status, setStatus] = useState(false);
+
+  return(
+    <View style={styles6.container}>
+      <Switch 
+        value={status}
+        onValueChange={ (valor) => setStatus(valor)}
+        trackColor={{false: "#121212", true: "#121212"}}
+        thumbColor={status ? "#00ff00" : "#ff0000"}
+      />
+      <Text style={styles5.text}>Status : {String(status) === "true" ? "Ativo" : "Inativo"}</Text>
+    </View>
+  )
+}
+const styles6 = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 30
+  },
+})
+
+
+
+
+
+function slider() {
+  const [kg, setKg] = useState(50);
+
+  return(
+    <View style={styles5.container}>
+      <Slider 
+        minimumValue={0} 
+        maximumValue={100} 
+        value={kg} 
+        onValueChange={(kg) => setKg(kg)} 
+        minimumTrackTintColor="#0000ff" 
+        maximumTrackTintColor="#ff0000" 
+        thumbTintColor="#0000ff"
+      />
+
+      <Text style={styles5.text}>KG : {kg.toFixed(2)}</Text>
+    </View>
+  )
+}
+const styles5 = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 30
+  },
+  text: {
+    color: "#000",
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 15
+  }
+})
+
+
+
+function picker() {
+  const [carSelected, setCarSelected] = useState(0);
+  const [cars , setCars] = useState([
+    {key: 0,nome: "Golf",valor: 30500},
+    {key: 1,nome: "BMW",valor: 200000},
+    {key: 2,nome: "Fiat",valor: 20000},
+    {key: 3,nome: "Saveiro",valor: 27000},
+    {key: 4,nome: "Ferrari",valor: 3000000},
+  ]);
+
+  let carsItems = cars.map((v,k) => {
+    return <Picker.Item key={k} value={k} label={v.nome} />
+  })
+
   return(
     <View style={styles4.container}>
-      
+      <Picker selectedValue={carSelected} onValueChange={ (itemValue , itemIndex) => setCarSelected(itemValue)}>
+        {carsItems}
+      </Picker>
+
+      <Text >{cars[carSelected].nome}</Text>
     </View>
   )
 }
@@ -15,9 +94,6 @@ const styles4 = StyleSheet.create({
     flex: 1
   }
 })
-
-
-
 
 
 
